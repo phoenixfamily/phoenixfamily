9c633818a1d47cf1a0882212d0613296d8d60c8c
abbaslotfinasab <abbas.lotfinasab2025@gmail.com>
2025-09-11 19:10:45 +0330

    login page added

--- WebMail/forms.py
+++ WebMail/forms.py
@@ -1,10 +1,19 @@
+# webmail/forms.py
 from django import forms
 from .models import MailAccount
 
 class MailAccountForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput(), required=True)
+
     class Meta:
         model = MailAccount
-        fields = ["email", "username", "password", "imap_host", "imap_port", "smtp_host", "smtp_port", "is_active", "is_default"]
-        widgets = {
-            "password": forms.PasswordInput(render_value=True),
-        }
+        fields = ["email", "username", "password", "imap_host", "imap_port", "smtp_host", "smtp_port", "use_ssl", "is_active", "is_default"]
+
+    def save(self, commit=True):
+        account = super().save(commit=False)
+        raw_password = self.cleaned_data.get("password")
+        if raw_password:
+            account.set_password(raw_password)
+        if commit:
+            account.save()
+        return account
